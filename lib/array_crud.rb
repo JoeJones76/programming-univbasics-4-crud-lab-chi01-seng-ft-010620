a254ae50faebbeb34cd04e0e66c2a693bfc08ab9
essential-workshop-9972 <josephjones7@gmail.com>
2020-01-03 02:56:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,19 +7,19 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << ("dog")
+  array << "dog"
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift ("frog")
+  array.unshift 
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop ()
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  array.shift
   
 end
 
